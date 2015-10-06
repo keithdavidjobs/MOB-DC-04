770ca5a0b1812259e251ba069220412a06c85a78
keithdavidjobs <Shalev@Shalevs-MacBook-Air.local>
2015-10-05 20:40:35 -0400

    Saving classwork.

---
+++ Classwork/Lesson03/introToSwift/introToSwift/introToSwift/introToSwift.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Decimal/fractions are known as Floats and Doubles. Doubles are more precise.
+//Default it a double if type is not specified
+
+
+var complexNum: Float = 3.1
+
+print("hello with terminator", terminator: "")
